0d5f6784ede5a4327c10284ce99989f58f876f94
maxly20 <maxly2011@gmail.com>
2021-06-07 11:48:31 +1000

    building GraphQL API

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
-import Amplify from 'aws-amplify';
+import { useState, useEffect } from 'react';
+import Amplify, { API, graphqlOperation } from 'aws-amplify';
 import awsconfig from './aws-exports';
 import { AmplifySignOut, withAuthenticator } from '@aws-amplify/ui-react';
+import { listSongs } from './graphql/queries';
 
 Amplify.configure(awsconfig);
 
 const App = () => {
+  const [songs, setSongs] = useState([]);
+
+  useEffect(() => {
+    fetchSongs();
+  }, []);
+
+  const fetchSongs = async () => {
+    try {
+      const songData = await API.graphql(graphqlOperation(listSongs));
+      const songList = songData.data.listSongs.items;
+      console.log('song list', songList);
+      setSongs(songList);
+    } catch (error) {
+      console.log('error on fetching songs', error);
+    }
+  };
+
   return (
     <div className='app'>
       <header className='app__header'>
