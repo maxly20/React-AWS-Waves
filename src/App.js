9f525667732b8926bf6f0d0021ca1338bc80972a
maxly20 <maxly2011@gmail.com>
2021-06-07 11:20:53 +1000

    config amplify withAuthenticator

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import Amplify from 'aws-amplify';
+import awsconfig from './aws-exports';
+import { AmplifySignOut, withAuthenticator } from '@aws-amplify/ui-react';
+
+Amplify.configure(awsconfig);
+
 const App = () => {
-  return <div className='App'>HELLLOOOOO</div>;
+  return (
+    <div className='app'>
+      <header className='app__header'>
+        <AmplifySignOut />
+        <h2>My App Content</h2>
+      </header>
+    </div>
+  );
 };
 
-export default App;
+export default withAuthenticator(App);
